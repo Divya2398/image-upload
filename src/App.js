8293a6e08e8d0621149f282770fbe81d4ff205bd
divya2398 <divya.platosys@gmail.com>
2022-07-08 23:01:18 +0530

    upload-frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [image, setImage] = useState("");
+  const handlechange = (event) => {
+    // console.log(event.target.value);C:\fakepath\photo.jpng.jpg
+    // console.log(URL.createObjectURL(event.target.files[0])); //http://localhost:3000/8c070bac-aea9-488d-bab9-eabdd5a8953a
+    setImage(event.target.files[0]);
+  };
+  const submit = () => {
+    // console.log("testing");
+    const formdata = new FormData();
+    formdata.append("file", image);
+    formdata.append("data", { name: "divya" });
+    axios
+      .post("http://localhost:3300/image-upload", formdata, {
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+      .then((Response) => {
+        console.log("res", Response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="offset-lg-3 col-lg-5 mt-5 p-5 border">
+          <form>
+            <div>
+              <label htmlFor="imginput" className="form-label">
+                upload your Image
+              </label>
+              <input
+                type="file"
+                id="imginput"
+                className="form-control"
+                onChange={handlechange}
+              />
+            </div>
+            <button
+              className="btn btn-success w-100 mt-3"
+              type="button"
+              onClick={submit}
+            >
+              Upload
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
